8f665fb53d96b51a54e719990f2655e402dde72c
emirhanbabu25 <70066975+emirhanbabu25@users.noreply.github.com>
2020-08-26 18:35:47 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "631545259297603625", //kendi IDınızı yazınız
+    "admins": ["631545259297603625"],
+    "support": ["631545259297603625"],
+    "token": "NzQ3MTIwNDIwNTIzODY4MjYx.X0KQHQ.fkwf9LtcQu1Moj-mH6XNPf8s1pM", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "", //botunuzun secretini yazınız
+      "callbackURL": ``, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "", //kalsın
+      "domain": "", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
